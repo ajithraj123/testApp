f7547a50d7e40185d4cf68a08e06d1ae504c6eb9
Lijoosh <lijooshc@gmail.com>
2015-08-05 16:31:11 +0530

    Modification in main class

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
         System.out.println("First modification");
+	System.out.println("Modification added by Lijoosh");
     }
 
 }
